85fe26a19d5c5f189baf85231e89142ab6629e5a
Jakub Biesiada <biesiadajakub@icloud.com>
2019-05-27 22:01:04 +0200

    Add copy/paste/cut & screen values fit

--- src/components/Screens/index.js
+++ src/components/Screens/index.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { fillParentContainer } from 'fancy-textfill';
 import PropTypes from 'prop-types';
 
 import './Screen.scss';
 
-const Screen = ({ className = '', value }) => (
-  <section className={`screen ${className}`}>{value}</section>
-);
+const clipboard = window.clipboard;
+
+const Screen = ({ className = '', value }) => {
+  const ref = useRef();
+
+  const [screenValue, setScreenValue] = useState(0);
+
+  useEffect(() => {
+    setScreenValue(value);
+
+    ref.current.addEventListener('click', e => {
+      const text = e.target.innerText;
+
+      document.addEventListener('copy', () => {
+        clipboard.writeText(text);
+      });
+
+      document.addEventListener('paste', () => {
+        if (!isNaN(clipboard.readText())) {
+          setScreenValue(clipboard.readText());
+        }
+      });
+
+      document.addEventListener('cut', () => {
+        clipboard.writeText(text);
+        setScreenValue(0);
+      });
+    });
+  }, [value]);
+
+  useEffect(() => {
+    fillParentContainer(ref.current, {
+      minFontSize: 14,
+      maxFontSize: 30,
+      maxWidth: ref.current.offsetWidth,
+      multiline: false
+    });
+  }, [screenValue]);
+
+  return (
+    <section className={`screen ${className}`} ref={ref}>
+      {screenValue}
+    </section>
+  );
+};
 
 Screen.propTypes = {
   className: PropTypes.string,
