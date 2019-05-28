a87d7553e9c8149fd0b0a80be4f6e0d94a0ce3a8
Jakub Biesiada <biesiadajakub@icloud.com>
2019-05-28 22:57:15 +0200

    Add window icons for Windows

--- src/components/Drag/index.js
+++ src/components/Drag/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 
 import './Drag.scss';
 
-const Drag = () => <div className="drag" />;
+const remote = window.remote;
+
+const main = remote.getCurrentWindow();
+
+const Drag = () => (
+  <div className="drag">
+    {navigator.platform.toLowerCase() === 'win32' && (
+      <div>
+        <button onClick={() => main.minimize()}>
+          <span>&#xE921;</span>
+        </button>
+
+        <button onClick={() => main.close()}>
+          <span>&#xE8BB;</span>
+        </button>
+      </div>
+    )}
+  </div>
+);
 
 export default Drag;
