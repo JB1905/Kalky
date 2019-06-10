bf860cecd548124c296da523fb26142067b97ae3
Jakub Biesiada <biesiadajakub@icloud.com>
2019-06-10 20:53:07 +0200

    Update css classes

--- src/components/Select/Currency.js
+++ src/components/Select/Currency.js
@@ -21,7 +21,7 @@ export default function Currency({ onChange, units, offset }) {
   }, [units, offset, selected]);
 
   return (
-    <select value={selected} onChange={choose}>
+    <select className="select__input" value={selected} onChange={choose}>
       {possibilities.map(possibility => (
         <option key={possibility} value={possibility}>
           {possibility}
